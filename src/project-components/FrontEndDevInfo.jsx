4d098f34dfe46a5d19747a87552748f7542c285f
2020mtran <2020mtran@gmail.com>
2025-01-04 13:01:09 -0800

    Second project page completed.

--- src/project-components/FrontEndDevInfo.jsx
+++ src/project-components/FrontEndDevInfo.jsx
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./FrontEndDevInfo.css";
 import FrontEndDevFirstPreview from "../assets/FrontEndDevFirstPreview.png";
 import FrontEndDevSecondPreview from "../assets/FrontEndDevSecondPreview.png";
 import FrontEndDevThirdPreview from "../assets/FrontEndDevThirdPreview.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare, faCircleLeft, faCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const FrontEndDevInfo = () => {
+    const images = [
+        { 
+            src: FrontEndDevFirstPreview,
+            title: 'Landing Page',
+            demoLink: 'https://2020mtran.github.io/odin-landing/',
+            repoLink: 'https://github.com/2020mtran/odin-landing',
+        },
+        {
+            src: FrontEndDevSecondPreview,
+            title: 'Sign-Up Page',
+            demoLink: 'https://2020mtran.github.io/odin-signup/',
+            repoLink: 'https://github.com/2020mtran/odin-signup',
+        },
+        {
+            src: FrontEndDevThirdPreview,
+            title: 'Admin Dashboard',
+            demoLink: 'https://2020mtran.github.io/odin-admin/',
+            repoLink: 'https://github.com/2020mtran/odin-admin',
+        },
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextImage = () => {
+        setCurrentIndex((currentIndex+1) % images.length);
+    }
+
+    const lastImage = () => {
+        currentIndex === 0 ? setCurrentIndex(images.length - 1) : setCurrentIndex(currentIndex - 1);
+    }
+
     return (
         <div className="fed-project-page-info">
-            <img src={FrontEndDevFirstPreview} alt="First preview image" className='fed-project-image'></img>
+            <div className='fed-image-container'>
+                <button className='fed-last-button' onClick={lastImage}>
+                    <FontAwesomeIcon icon={faCircleLeft} className='fed-arrow'/>
+                </button>
+                <img src={images[currentIndex].src} alt="Preview image" className='fed-project-image'></img>
+                <button className='fed-next-button' onClick={nextImage}>
+                    <FontAwesomeIcon icon={faCircleRight} className='fed-arrow'/>
+                </button>
+            </div>
             <div className='fed-project-display-info'>
-                <p className='fed-display-title'>Landing Page</p>
+                <p className='fed-display-title'>{images[currentIndex].title}</p>
                 <div className='icon-row'>
-                    <a href="https://2020mtran.github.io/odin-landing/" target="_blank" rel="noopener norefferer" className='fed-button'>
+                    <a href={images[currentIndex].demoLink} target="_blank" rel="noopener norefferer" className='fed-button'>
                         <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='out-icon'/>
                     </a>
-                    <a href="https://github.com/2020mtran/odin-landing" target="_blank" rel="noopener norefferer" className='fed-button'>
+                    <a href={images[currentIndex].repoLink} target="_blank" rel="noopener norefferer" className='fed-button'>
                         <FontAwesomeIcon icon={faGithub} className="fed-github-icon"/>
                     </a>
                 </div>
